c2f01f4c19d2f859084adf6bbd5f8012d97ce0be
mr-ilin <mr.ilin14@gmail.com>
2021-10-22 13:32:25 +0300

    Initial Commit

---
+++ Stack top Qs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stack top Qs
+//
+//  Created by Илья Ильин on 22.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
